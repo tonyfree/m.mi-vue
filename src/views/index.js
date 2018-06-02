45af5a56cdb3582a4339b8c29d1ea89d5a390402
tony <fangtao_2008@163.com>
2018-06-02 15:16:15 +0800

    copy MiHome.vue to views

--- src/views/index.js
+++ src/views/index.js
@@ -1,12 +1,13 @@
-import Home from './Home.vue'
+// import Home from './Home.vue'
 import About from './About.vue'
 import MiLogin from './MiLogin.vue'
+import MiHome from './MiHome.vue'
 
 export default [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: MiHome
   },
   {
     path: '/about',
