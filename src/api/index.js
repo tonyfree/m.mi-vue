1b597b116e53d0989eac1384bd76577079d26823
tony <fangtao_2008@163.com>
2018-06-02 15:11:34 +0800

    add url:navList

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,8 @@ const host = 'http://rap2api.taobao.org/app/mock/13801'
 
 const url = {
   login: '/api/login',
-  getCode: '/api/getCode'
+  getCode: '/api/getCode',
+  navList: 'api/navList'
 }
 
 Object.keys(url).forEach(key => {
