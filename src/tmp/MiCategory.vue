be7a6a6cb1f073b5f11bcc37d0ec90d96ff4091e
tony <fangtao_2008@163.com>
2018-06-07 21:54:31 +0800

    update html and style

--- src/tmp/MiCategory.vue
+++ src/tmp/MiCategory.vue
@@ -107,8 +107,8 @@
           <div class="list-item category0">
             <div class="component-list-main">
               <div class="cells_auto_fill" index="0">
-                <a class="exposure items" style="width: 5rem; height: 2rem;">
-                  <img src="//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/711275806886438be20e33ec943a904a.jpg?thumb=1&amp;w=500&amp;h=200" lazy="loaded" style="height: 2rem; width: 5rem;">
+                <a class="exposure items">
+                  <img src="//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/711275806886438be20e33ec943a904a.jpg?thumb=1&amp;w=500&amp;h=200" lazy="loaded">
                 </a>
               </div>
               <div name="m1" class="category_title">
@@ -119,7 +119,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/cae1e9af28aebb1d0e065cbdc08d8f66!120x120.png" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/cae1e9af28aebb1d0e065cbdc08d8f66!120x120.png" lazy="loaded">
                       </div>
                       <div class="name">小米8</div>
                     </a>
@@ -127,7 +127,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/dc6165304dcced2a3b2cc574e4af9b4a!120x120.png" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/dc6165304dcced2a3b2cc574e4af9b4a!120x120.png" lazy="loaded">
                       </div>
                       <div class="name">小米8 SE</div>
                     </a>
@@ -135,7 +135,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/06cabb01bbe4ac5c819ee101e4446a16!120x120.png" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/06cabb01bbe4ac5c819ee101e4446a16!120x120.png" lazy="loaded">
                       </div>
                       <div class="name">小米MIX 2S</div>
                     </a>
@@ -143,7 +143,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/745cd6cbffce6a1c1c1cafbd5abbfed0!120x120.png" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/745cd6cbffce6a1c1c1cafbd5abbfed0!120x120.png" lazy="loaded">
                       </div>
                       <div class="name">黑鲨游戏手机</div>
                     </a>
@@ -151,7 +151,7 @@
                   <div class="product">
                     <a class="exposure item" data-stat-id="dd150121ab5f594b">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/74049580f582c72efdde7c80fbdbd223!120x120.jpg" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/74049580f582c72efdde7c80fbdbd223!120x120.jpg" lazy="loaded">
                       </div>
                       <div class="name">红米S2</div>
                     </a>
@@ -166,7 +166,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/bf2dc71cbf6fa8413b7ade09719155a0!120x120.jpg" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/bf2dc71cbf6fa8413b7ade09719155a0!120x120.jpg" lazy="loaded">
                       </div>
                       <div class="name">75英寸 电视4</div>
                     </a>
@@ -174,7 +174,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/07037fbe905f42477d4816ee64d857b5!120x120.jpg" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/07037fbe905f42477d4816ee64d857b5!120x120.jpg" lazy="loaded">
                       </div>
                       <div class="name">55"4S 曲面</div>
                     </a>
@@ -182,7 +182,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/4129babe8bee3dd1c9ddcbb112b319f5!120x120.jpg" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/4129babe8bee3dd1c9ddcbb112b319f5!120x120.jpg" lazy="loaded">
                       </div>
                       <div class="name">55英寸 4X</div>
                     </a>
@@ -190,7 +190,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/fa50f303fa906aa00af309fa5b10dda8!120x120.jpg" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/fa50f303fa906aa00af309fa5b10dda8!120x120.jpg" lazy="loaded">
                       </div>
                       <div class="name">32英寸 4C</div>
                     </a>
@@ -198,7 +198,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/febe6b5c777b1d91e08d44b62d71092d!120x120.jpg" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/febe6b5c777b1d91e08d44b62d71092d!120x120.jpg" lazy="loaded">
                       </div>
                       <div class="name">43英寸 4S</div>
                     </a>
@@ -206,7 +206,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/48437e4f9e6d65d5adfdd51add78b59b!120x120.jpg" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/48437e4f9e6d65d5adfdd51add78b59b!120x120.jpg" lazy="loaded">
                       </div>
                       <div class="name">43英寸 青春版</div>
                     </a>
@@ -221,7 +221,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/ccad3ac9a43a21a606a392cf6a14b091!120x120.jpg" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/ccad3ac9a43a21a606a392cf6a14b091!120x120.jpg" lazy="loaded">
                       </div>
                       <div class="name">15.6"游戏本</div>
                     </a>
@@ -229,7 +229,7 @@
                   <div class="product">
                     <a  class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/dce7af666be089a9a69d23559c0d180c!120x120.png" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/dce7af666be089a9a69d23559c0d180c!120x120.png" lazy="loaded">
                       </div>
                       <div class="name">13.3"增强版</div>
                     </a>
@@ -237,7 +237,7 @@
                   <div class="product">
                     <a class="exposure item">
                       <div class="img">
-                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/5c14cc0183352b8d6f4751a84016756c!120x120.png" lazy="loaded" style="width: 1.2rem; height: 1.2rem;">
+                        <img class="big" src="//i8.mifile.cn/b2c-mimall-media/5c14cc0183352b8d6f4751a84016756c!120x120.png" lazy="loaded">
                       </div>
                       <div class="name">12.5"office版</div>
                     </a>
@@ -300,25 +300,86 @@
 }
 .container .list-wrap {
   position: absolute;
-  left: 1.52rem;
+  left: 80px;
   right: 0;
   top: 49px;
   bottom: 52px;
-  padding: 2px .32rem;
+  padding: 2px 16px;
   overflow: auto;
 }
 .component-list-main .cells_auto_fill .items {
   height: auto!important;
   display: block;
 }
+.cells_auto_fill .items img{
+  width: 260px; 
+  height: 104px;
+}
 .component-list-main .category_title {
   background: #fff;
-  font-size: .28rem;
+  font-size: 16px;
   text-align: center;
   font-weight: 400;
-  margin-top: .2rem;
-  height: 1.28rem;
-  line-height: 1.28rem;
+  margin-top: 10px;
+  height: 66px;
+  line-height: 66px;
   overflow: hidden;
+}
+.component-list-main .category_title span {
+  position: relative;
+}
+.component-list-main .category_title span:after, .component-list-main .category_title span:before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 0;
+  width: 30px;
+  border-top: 1px solid #e0e0e0;
+  transform: translate3d(-150%,-50%,0);
+}
+.component-list-main .category_title span:after {
+  left: auto;
+  right: 0;
+  transform: translate3d(150%,-50%,0);
+}
+.component-list-main .category_group .box {
+  width: 100%;
+  overflow: hidden;
+}
+.box-flex>*, .box-inline-flex>* {
+  flex: 1 1 auto;
+}
+.component-list-main .category_group .product {
+  float: left;
+  width: 33.333333333333336%;
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 15px;
+  overflow: hidden;
+}
+.component-list-main .category_group .product .img {
+  width: 52px;
+  height: 52px;
+  margin: 0 auto;
+  background: #fff;
+  overflow: hidden;
+}
+.component-list-main .img {
+  display: block;
+  position: relative;
+  overflow: hidden;
+}
+.component-list-main .category_group .product .img img {
+  width: 100%;
+}
+.category_group .big {
+  height: 100%!important;
+  width: auto!important;
+}
+.component-list-main .category_group .name {
+  margin-top: 14px;
+  white-space: nowrap;
+  font-size: 12px;
+  color: rgba(0,0,0,.54);
 }
 </style>
