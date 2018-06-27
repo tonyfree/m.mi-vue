501f1d8f2546168d4bc28e5e8451adf864d09c3b
tony <fangtao_2008@163.com>
2018-06-27 08:49:49 +0800

    MiCart.vue template

--- src/App.vue
+++ src/App.vue
@@ -52,17 +52,7 @@ export default {
   text-size-adjust: none;
 }
 
-/* 通用样式 */
-.app-shell {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
+
 .app-view-wrapper {
   position: relative;
   overflow: hidden;
