8d4898552c7f03ea15c8fd0ddf163af3981632ae
tony <fangtao_2008@163.com>
2018-06-30 17:31:21 +0800

    add mock:cartIndex.js

---
+++ src/mock/cartIndex.js
@@ -0,0 +1,161 @@
+/* eslint-disable */
+import Mock from 'mockjs'
+
+let cartIndex = Mock.mock({
+  "code": 0,
+  "result": "ok",
+  "description": "success",
+  "data": {
+    "items": [
+      {
+        "goodsId": "2181100015",
+        "image_url": "//i1.mifile.cn/a1/pms_1522318330.86967810!180x1800.jpg",
+        "buy_limit": 5,
+        "sel_status": 0,
+        "product_name": "小米电视4C 50英寸 黑色 50英寸",
+        "price": 1999,
+        "num": 1,
+        "service_info": [
+          {
+            "can_multi": false,
+            "type_name": "会员服务",
+            "service_url": "//cdn.cnbj0.fds.api.mi-img.com/b2c-data-mishop/72d5bc482304.html",
+            "service_info": [
+              {
+                "service_goods_id": "2182000044",
+                "service_image_url": "//i1.mifile.cn/a1/T1TexjBsKT1RXrhCrK.jpg",
+                "service_short_name": "影视会员年卡",
+                "service_price": 299,
+                "sel_status": 0
+              }
+            ]
+          },
+          {
+            "can_multi": false,
+            "type_name": "安装服务",
+            "service_url": "//cdn.cnbj0.fds.api.mi-img.com/b2c-data-mishop/a739137749cc.html",
+            "service_info": [
+              {
+                "service_goods_id": "81000020071102471X",
+                "service_image_url": "//i1.mifile.cn/a1/pms_1523433952.48583019.jpg",
+                "service_short_name": "电视座装服务",
+                "service_price": 80,
+                "sel_status": 0
+              },
+              {
+                "service_goods_id": "990000197006122668",
+                "service_image_url": "//i1.mifile.cn/a1/pms_1523434179.92866217.jpg",
+                "service_short_name": "电视挂装服务",
+                "service_price": 190,
+                "sel_status": 0
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "goodsId": "630000199612247492",
+        "image_url": "//i1.mifile.cn/a1/pms_1527060176.9632373!180x1800.jpg",
+        "buy_limit": 5,
+        "sel_status": 1,
+        "product_name": "小米电视4X 55英寸 黑色 55英寸",
+        "num": 1,
+        "service_info": [
+          {
+            "can_multi": false,
+            "type_name": "会员服务",
+            "service_url": "//cdn.cnbj0.fds.api.mi-img.com/b2c-data-mishop/72d5bc482304.html",
+            "service_info": [
+              {
+                "service_goods_id": "2182000044",
+                "service_image_url": "//i1.mifile.cn/a1/T1TexjBsKT1RXrhCrK.jpg",
+                "service_short_name": "影视会员年卡",
+                "service_price": 299,
+                "sel_status": 0
+              }
+            ]
+          },
+          {
+            "can_multi": false,
+            "type_name": "安装服务",
+            "service_url": "//cdn.cnbj0.fds.api.mi-img.com/b2c-data-mishop/a739137749cc.html",
+            "service_info": [
+              {
+                "service_goods_id": "81000020071102471X",
+                "service_image_url": "//i1.mifile.cn/a1/pms_1523433952.48583019.jpg",
+                "service_short_name": "电视座装服务",
+                "service_price": 80,
+                "sel_status": 1
+              },
+              {
+                "service_goods_id": "990000197006122668",
+                "service_image_url": "//i1.mifile.cn/a1/pms_1523434179.92866217.jpg",
+                "service_short_name": "电视挂装服务",
+                "service_price": 190,
+                "sel_status": 0
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "goodsId": "330000197302099463",
+        "image_url": "//i1.mifile.cn/a1/pms_1524621084.0039673!180x1800.jpg",
+        "buy_limit": 2,
+        "sel_status": 0,
+        "product_name": "小米6X 全网通版 4GB内存 64GB 流沙金",
+        "price": 1599,
+        "num": 1,
+        "service_info": [
+          {
+            "can_multi": false,
+            "type_name": "保障服务",
+            "service_url": "//cdn.cnbj0.fds.api.mi-img.com/b2c-data-mishop/4a15d767c1fe.html",
+            "service_info": [
+              {
+                "service_goods_id": "650000197903152244",
+                "service_image_url": "//i1.mifile.cn/a1/pms_1524032278.1757986.jpg",
+                "service_short_name": "意外保障服务",
+                "service_price": 179
+              }
+            ]
+          }
+        ],
+        "gift": [{
+          "product_name": "小米蓝牙耳机青春版 白色",
+          "image_url": "//i1.mifile.cn/a1/pms_1478248761.54944181.jpg",
+          "actId": "820000200604066342"
+        }]
+      },
+      {
+        "goodsId": "340000198205095723",
+        "image_url": "//i1.mifile.cn/a1/pms_1524621078.90015819!180x1800.jpg",
+        "buy_limit": 2,
+        "sel_status": 1,
+        "product_name": "",
+        "price": 1599,
+        "num": 2,
+        "service_info": [
+          {
+            "can_multi": false,
+            "type_name": "保障服务",
+            "service_url": "//cdn.cnbj0.fds.api.mi-img.com/b2c-data-mishop/4a15d767c1fe.html",
+            "service_info": [
+              {
+                "service_goods_id": "650000197903152244",
+                "service_image_url": "//i1.mifile.cn/a1/pms_1524032278.1757986.jpg",
+                "service_short_name": "意外保障服务",
+                "service_price": 179
+              }
+            ]
+          }
+        ],
+        "gift": [{
+          "product_name": "小米蓝牙耳机青春版 白色",
+          "image_url": "//i1.mifile.cn/a1/pms_1478248761.54944181.jpg",
+          "actId": "460000199402246828"
+        }]
+      }
+    ]
+  }
+})
