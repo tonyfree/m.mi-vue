204f4ac20a4a7d9d5d37f78af19ee5c95d4a977a
tony <fangtao_2008@163.com>
2018-07-02 09:06:06 +0800

    merge

--- src/views/MiCart.js
+++ src/views/MiCart.js
@@ -1,7 +1,6 @@
 import { cartIndex } from '@/mock/cart.js'
 import MiSearch from '@/components/MiSearch.vue'
 import fetch from '@/api/fetch.js'
-import { SIGTERM } from 'constants';
 
 export default {
   components: {
@@ -56,11 +55,12 @@ export default {
           buy_limit: items[index].num,
           goodsId: info.service_goods_id,
           image_url: info.service_image_url,
-          num: items[index].num,
+          num: info.num,
           price: info.service_price,
           product_name: info.service_short_name,
           sel_status: 1,
-          parent_goodsId: info.parent_goodsId
+          parent_goodsId: info.parent_goodsId,
+          isService: true
         })
       })
       let giftSelect = []
@@ -85,7 +85,8 @@ export default {
           num: items[index].num,
           product_name: gift.product_name,
           sel_status: 1,
-          parent_goodsId: gift.parent_goodsId
+          parent_goodsId: gift.parent_goodsId,
+          isGift: true
         })
       })
       this.cartList = items
